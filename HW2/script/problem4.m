e12382b2fb32c7416bd635d3428282e76c29ab22
Garyandtang <a750695325@gmail.com>
2020-09-27 16:00:00 +0800

    update_problem_4

--- HW2/script/problem4.m
+++ HW2/script/problem4.m
@@ -1,21 +1,25 @@
 clear;clc;
-n = randi([1,100],1);
-p = randi([1,n-1],1);
+n = 10
+p = 2
 X = zeros(n,p);
 cMatrix = eye(n);
-a = 0;
+a = 2;
 for i = 1:p
     noiseVec = randn(n,1);
     noiseVec = cMatrix*noiseVec;
     X(:,i) = noiseVec;
 end
-
+num = 100;
 S = 1/n * (X')*X;
+theta_1_norm = zeros(num,1);
+for i = 1 : num
+    cvx_begin
+        variable theta(p,p) semidefinite
+        minimize(trace(S*theta) - log_det(theta*S) + (i-1)*ones(1,p)*theta*ones(p,1))
+    cvx_end
+    theta_1_norm(i) = ones(1,p)*theta*ones(p,1);
+end
 
-cvx_begin
-    variable theta(p,p) semidefinite
-    minimize(trace(S*theta) - log_det(theta*S)+ a*ones(1,p)*theta*ones(p,1))
-    subject to
-        theta <In> semidefinite(p);
-cvx_end
-        
+a = 0 : num-1;
+plot(a,theta_1_norm);
+
