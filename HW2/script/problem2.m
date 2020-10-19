6e1a3bdb293c2f68b343ced1ba411a419ae1d31f
Garyandtang <a750695325@gmail.com>
2020-10-19 09:52:27 +0800

    sth

--- HW2/script/problem2.m
+++ HW2/script/problem2.m
@@ -6,7 +6,7 @@ n = 3;
 
 lambda  = zeros(11,1);
 for i = 1:11
-    lambda(i) = (i-1)*10^4;
+    lambda(i) = (i-1)*10^-4;
 end
 
 w_optimal = zeros(11,3);
@@ -25,4 +25,4 @@ volatility = zeros(11,1);
 for i = 1:11
     volatility(i)=sqrt(w_optimal(i,:)*sigma*w_optimal(i,:)');
 end
-plot(expected_return,sqrt(volatility))
+plot(expected_return,volatility)
---
+++ HW3/problem2.m
@@ -0,0 +1,31 @@
+clc;clear;
+rng(1);
+n = 5;
+Y = complex(normrnd(0, 1, n, n*2), normrnd(0, 1, n, n*2));
+M = Y*Y';
+
+% get maximum eigenvalue of M
+[U, Sigma, V] = svd(M);
+max_lambda = max(max(Sigma));
+
+% MM algorithm
+k = 1;
+x = ones(n,1);
+obj_value = [x'*M*x];
+while 1
+    x_ = x;
+    y = -1*(max_lambda*eye(n) - M)*x;
+    for i = 1 : n
+        x(i) = -exp(angle(y(i))*1i);
+    end
+    
+    obj_value = [obj_value; x'*M*x];
+    k = k +1;
+    if abs(x_ - x) <= 0.000001
+        break
+    end
+end
+
+plot([1:k],obj_value);
+xlabel({'The numbber of iterations'},'Interpreter','latex');
+ylabel({'Objective value'},'Interpreter','latex');
